5f9b2ace966e81a0dccb44d785d185430f518a15
FLIGHNE <124429984+FLIGHNE@users.noreply.github.com>
2023-02-11 20:27:38 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ obj.innerText = text;
 function changeBgColor(obj, colorName) {
     obj.style.backgroundColor = colorName;
 }
+
+
+function scaleUp(obj, size) {
+    obj.style.fontSize = size;
+}
+
+function applyTheme(theme) {
+    document.body.classList.remove("theme-auto", "theme-light", "theme-dark");
+    document.body.classList.add(`theme-${theme}`);
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme") || "auto";
+  
+    applyTheme(savedTheme);
+  
+    for (const optionElement of document.querySelectorAll("#selTheme option")) {
+      optionElement.selected = savedTheme === optionElement.value;
+    }
+  
+    document.querySelector("#selTheme").addEventListener("change", function () {
+      localStorage.setItem("theme", this.value);
+      applyTheme(this.value);
+    });
+  });
